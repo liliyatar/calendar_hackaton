9dade7189192b5c71ce009f179fc38088afd72cc
Liliya Taran <liliyataran2017@gmail.com>
2020-01-19 08:52:52 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import './App.css';
+import './App.scss';
 import { Months } from './components/calendar/Months';
 import Month from './components/calendar/Month';
 import DateForm from './components/DateForm';
@@ -8,8 +9,6 @@ import EventForm from './components/EventForm';
 import {
     getYear,
     getMonth,
-    getDay,
-    setDay,
     setMonth,
     setYear,
     setWeek,
@@ -30,108 +29,120 @@ const App = ({
         const datePlus = new Date(year, month + 1);
         const dateMinus = new Date(year, month - 1);
         const tempDate = param === -1 ? dateMinus : datePlus;
-        setYear(tempDate.getFullYear());
+
+    setYear(tempDate.getFullYear());
         setMonth(tempDate.getMonth());
     };
 
-    const changeYear = (param) => {
-        setYear(param === 1 ? year + 1 : year - 1);
-    };
+  const changeYear = (param) => {
+      setYear(param === 1 ? year + 1 : year - 1);
+  };
 
-    const changeWeek = (param) => {
-        if (param === 1) {
-            setWeek(week === 4 ? 0 : week + 1);
-        } else {
-            setWeek(week === 0 ? 4 : week - 1);
-        }
-    };
+  const changeWeek = (param) => {
+      if (param === 1) {
+          setWeek(week === 4 ? 0 : week + 1);
+      } else {
+          setWeek(week === 0 ? 4 : week - 1);
+      }
+  };
 
-    return (
-        <>
-            {isShownEventForm
-                ? <EventForm />
-                : (
-                    <>
-                        <DateForm />
-                        <div className="buttons">
-                            <button
-                                className="buttons__prevYear"
-                                type="button"
-                                onClick={() => changeYear(-1)}
-                                disabled={year === ''}
-                            >
-                                prevYear
-                            </button>
-                            {(year !== '' && (month || month === 0)) && (
-                                <button
-                                    className="buttons__prevMonth"
-                                    type="button"
-                                    onClick={() => changeMonth(-1)}
-                                >
-                                    prevMonth
-                                </button>
-                            )}
-                            {(year !== '' && (week || week === 0))  && (
-                                <button
-                                    className="buttons__prevWeek"
-                                    type="button"
-                                    onClick={() => changeWeek(-1)}
-                                >
-                                    prevWeek
-                                </button>
-                            )}
-                            {(year !== '' && (week || week === 0)) && (
-                                <button
-                                    className="buttons__nextWeek"
-                                    type="button"
-                                    onClick={() => changeWeek(1)}
-                                >
-                                    nextWeek
-                                </button>
-                            )}
-                            {(year !== '' && (month || month === 0)) && (
-                                <button
-                                    className="buttons__nextMonth"
-                                    type="button"
-                                    onClick={() => changeMonth(1)}
-                                >
-                                    nextMonth
-                                </button>
-                            )}
-                            <button
-                                className="buttons__nextYear"
-                                type="button"
-                                onClick={() => changeYear(1)}
-                                disabled={year === ''}
-                            >
-                                nextYear
-                            </button>
-                        </div>
-                        {year !== '' && month === '' && (
-                            <div className="App">
-                                <Months />
-                            </div>
-                        )}
-                        {(year !== '' && (month || month === 0)) && <Month month={month} />}
-                    </>
-                )}
-        </>
-    );
+  return (
+      <>
+          {isShownEventForm
+              ? <EventForm />
+              : (
+                  <>
+                      <DateForm />
+                      <div className="buttons">
+                          <button
+                              className="buttons__prevYear"
+                              type="button"
+                              onClick={() => changeYear(-1)}
+                              disabled={year === ''}
+                          >
+                              prevYear
+                          </button>
+                          {(year !== '' && (month || month === 0)) && (
+                              <button
+                                  className="buttons__prevMonth"
+                                  type="button"
+                                  onClick={() => changeMonth(-1)}
+                              >
+                                  prevMonth
+                              </button>
+                          )}
+                          {(year !== '' && (week || week === 0))  && (
+                              <button
+                                  className="buttons__prevWeek"
+                                  type="button"
+                                  onClick={() => changeWeek(-1)}
+                              >
+                                  prevWeek
+                              </button>
+                          )}
+                          {(year !== '' && (week || week === 0)) && (
+                              <button
+                                  className="buttons__nextWeek"
+                                  type="button"
+                                  onClick={() => changeWeek(1)}
+                              >
+                                  nextWeek
+                              </button>
+                          )}
+                          {(year !== '' && (month || month === 0)) && (
+                              <button
+                                  className="buttons__nextMonth"
+                                  type="button"
+                                  onClick={() => changeMonth(1)}
+                              >
+                                  nextMonth
+                              </button>
+                          )}
+                          <button
+                              className="buttons__nextYear"
+                              type="button"
+                              onClick={() => changeYear(1)}
+                              disabled={year === ''}
+                          >
+                              nextYear
+                          </button>
+                      </div>
+                      {year !== '' && month === '' && (
+                          <div className="App">
+                              <Months />
+                          </div>
+                      )}
+                      {(year !== '' && (month || month === 0)) && <Month month={month} />}
+                  </>
+              )}
+      </>
+  );
 };
 
 const mapStateToProps = state => ({
     year: getYear(state),
     month: getMonth(state),
-    day: getDay(state),
     week: getWeek(state),
+    setMonth: getMonth(state),
+    setYear: getYear(state),
+    setWeek: getWeek(state),
     isShownEventForm: getIsShownEventForm(state),
 });
 
 const mapDispatchToProps = {
-    setDay,
     setMonth,
     setYear,
     setWeek,
+};
+
+App.propTypes = {
+    year: (PropTypes.number || PropTypes.string).isRequired,
+    month: (PropTypes.number || PropTypes.string).isRequired,
+    week: (PropTypes.number || PropTypes.string).isRequired,
+    setMonth: PropTypes.func.isRequired,
+    setYear: PropTypes.func.isRequired,
+    setWeek: PropTypes.func.isRequired,
+    isShownEventForm: PropTypes.bool.isRequired,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
